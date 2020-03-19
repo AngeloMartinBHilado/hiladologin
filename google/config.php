9ea3aec746377b3429d0ab1a69788c69091d205b
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-03-19 17:14:56 +0800

    Update config.php

--- google/config.php
+++ google/config.php
@@ -2,9 +2,9 @@
 require_once 'vendor/autoload.php';
 
 $google_client = new Google_Client();
-$google_client->setClientId('862564044784-fbpmelidspimkssnl0etfdcsp76510nc.apps.googleusercontent.com');
-$google_client->setClientSecret('Gos4kW0_ZZAnmUdQMvsTCocA');
-$google_client->setRedirectUri('https://api-project-finals.herokuapp.com/index.php');
+$google_client->setClientId('662135407730-v4kqikodv1ihcr56sgtjpd2va3seks40.apps.googleusercontent.com');
+$google_client->setClientSecret('O-kRQixHH9hOQHTzSzVjyDrk');
+$google_client->setRedirectUri('https://hiladocordones.herokuapp.com/home.php');
 $google_client->addScope('email');
 $google_client->addScope('profile');
 
